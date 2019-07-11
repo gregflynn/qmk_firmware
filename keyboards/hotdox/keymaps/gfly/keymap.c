e947673f710707e3b630bebb23f0989313ba6589
Greg Flynn <gregflynn42@gmail.com>
2019-07-11 15:15:50 -0400

    more hotdox tweaks

--- keyboards/hotdox/keymaps/gfly/keymap.c
+++ keyboards/hotdox/keymaps/gfly/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Home |           | End  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  FN    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  | Enter  |
+ * |  FN    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
  * |--------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | VolUp| VolDn|       | Play | Next |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | PgUp |       | PgDn |        |      |
- *                                 | Space| super|------|       |------| Ctrl   | Space|
- *                                 |      |      |  _   |       |  '"  |        |      |
+ *                                 | Space| super|------|       |------| Enter  | Space|
+ *                                 |      |      |  _   |       |    |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -43,18 +43,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(SYMB),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSPO,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTL,        KC_LGUI,      KC_LALT,  KC_LEFT,KC_RGHT,
-                                                      KC_VOLU,KC_VOLD,
-                                                              KC_PGUP,
+                                                      KC_PGUP,KC_VOLU,
+                                                              KC_VOLD,
                                                KC_SPC,KC_LGUI,KC_UNDS,
         // right hand
              KC_EQL,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,   KC_BSPC,
              KC_END,      KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,   KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,KC_ENT,
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,KC_RSPC,
                                   KC_UP, KC_DOWN,KC_RALT,KC_RGUI,KC_RCTL,
-             KC_MPLY, KC_MNXT,
-             KC_PGDN,
-             KC_QUOT, KC_RCTL,KC_SPC
+             KC_MPLY, KC_PGDN,
+             KC_MNXT,
+             KC_RCTL, KC_ENT, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
