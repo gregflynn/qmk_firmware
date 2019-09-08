2078eda63dea6d2cb677663b6cc8d00ba5b13f3f
Greg Flynn <gregflynn42@gmail.com>
2019-09-08 15:39:45 -0400

    use the right slash for fn numpad

--- keyboards/hotdox/keymaps/gfly/keymap.c
+++ keyboards/hotdox/keymaps/gfly/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_F12,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_DEL,
        _______, _______, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 _______, KC_4,   KC_5,    KC_6,    KC_DOT,  _______,
-       _______, _______, KC_1,   KC_2,    KC_3,    KC_BSLS, _______,
+       _______, _______, KC_1,   KC_2,    KC_3,    _______, _______,
                          KC_EQL, KC_MINS, KC_PLUS,    KC_0, KC_BSPC,
        _______, _______,
        _______,
