c893613eed8dbf4b7fe033a0e63489480ec08e86
Greg Flynn <gregflynn42@gmail.com>
2019-10-23 15:37:22 -0400

    update top level left/right

--- keyboards/hotdox/keymaps/gfly/keymap.c
+++ keyboards/hotdox/keymaps/gfly/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  End |           |   ]  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl | super|  alt | Left | Right|                                       |  =+  |  -_  |  alt | super| ctrl |
+ *   | Ctrl | super|  alt |  [   |   ]  |                                       |  =+  |  -_  |  alt | super| ctrl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Play | VolUp|       | Prev | Next |
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_HOME,
         MO(SYMB),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSPO,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_END,
-        KC_LCTL,        KC_LGUI,      KC_LALT,  KC_LEFT,KC_RGHT,
+        KC_LCTL,        KC_LGUI,      KC_LALT,KC_LBRC,KC_RBRC,
                                                       KC_MPLY,KC_VOLU,
                                                               KC_VOLD,
                                                KC_SPC,KC_LGUI,MO(SYMB),
